34f0ba75513eaf1b6068e04d80b13265fd26a993
Delta483711 <59081538+Delta483711@users.noreply.github.com>
2020-11-08 15:04:06 +0000

    Added Player Material Color & Added MouseLook Script & Added MoveScript & other stuff for player movement

---
+++ WorldGame/Assets/MouseLook.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBody;
+
+    private float xRoation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRoation -= mouseY;
+        xRoation = Mathf.Clamp(xRoation, -90f, 90f);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+        transform.localRotation = Quaternion.Euler(xRoation, 0f, 0f);
+
+    }
+}
